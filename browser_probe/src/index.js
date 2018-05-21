f45f3e306782817f777fca27c9db4eafb19e15b9
hb <hb@ike-global.com>
2018-05-21 18:05:37 +0800

    监测js

--- browser_probe/src/index.js
+++ browser_probe/src/index.js
@@ -8,13 +8,6 @@ if (window.__ml) {
             uploadUserData(1);
         }, 10);
     });
-    // document.onreadystatechange = function () {
-    //     if (document.readyState === "complete") {
-    //         setTimeout(() => {
-    //             uploadUserData(1);
-    //         }, 1000);
-    //     }
-    // };
     //监听API
     (function (xhr) {
         // Capture request before any network activity occurs:
@@ -54,9 +47,29 @@ if (window.__ml) {
         window.__ml.apiStartTime = Date.now();
     }
 
-    //监听前端js
-    window.addEventListener('error', function (ex) {
-        // 一般事件的参数中会包含pos信息
-        console.log(ex);
-    });
+    // window.addEventListener('error', function(err){
+    //     console.log(err,1111111);
+    // });
+
+    /** 
+  * @param {String} errorMessage  错误信息 
+  * @param {String} scriptURI   出错的文件 
+  * @param {Long}  lineNumber   出错代码的行号 
+  * @param {Long}  columnNumber  出错代码的列号 
+  * @param {Object} errorObj    错误的详细信息，Anything 
+  */
+
+    window.onerror = function (err) {
+        console.log(err, 123);
+        // alert("an error");
+    }
+
+    // window.onerror = function (errorMessage, scriptURI, lineNumber, columnNumber, errorObj) {
+    //     console.log("错误信息：", errorMessage);
+    //     console.log("出错文件：", scriptURI);
+    //     console.log("出错行号：", lineNumber);
+    //     console.log("出错列号：", columnNumber);
+    //     console.log("错误详情：", errorObj);
+    // };
+
 }
--- browser_probe/src/sendData.js
+++ browser_probe/src/sendData.js
@@ -1,41 +1,55 @@
 
-import {params}from './util';
-import {performanceTime} from './performance';
-import {terminalInfo} from './terminalInfo';
+import { params } from './util';
+import { performanceTime } from './performance';
+import { terminalInfo } from './terminalInfo';
 
 //上报api数据
-/**
- *发送数据
- */
-function send(param){
-  console.log(param,123);
-  let img=new Image();
-  img.src=window.__ml&&window.__ml.config.imgUrl+params(param);
-  img.onload=img.onerror=function(){
-    img=undefined;
-  };
-}
-
-function uploadUserData(type,ext){
-  if(type==1){
-    sendPerfData();
+function uploadUserData(type, ext) {
+  switch (type) {
+    case 1:
+      sendPerfData()
+      break;
+    case 2:
+      sendApiData(ext)
+      break;
+    case 3:
+      sendJsErrData(ext)
+      break;
+    default:
+      console.log('未定义类型');
+      break;
   }
 
-  if(type==2){
-    sendApiData(ext);
-  }
 }
 
 // 发送性能数据
-function sendPerfData(){
-  var temp={type:'perf',page:location.host};
-  _.extend(temp,terminalInfo,performanceTime);
+function sendPerfData() {
+  var temp = { type: 'perf', page: location.host };
+  _.extend(temp, terminalInfo, performanceTime);
   send(temp);
 }
 // 发送api请求数据
-function sendApiData(ext){
-  var temp={type:'api',page:location.href};
-  _.extend(temp,terminalInfo,ext);
+function sendApiData(ext) {
+  var temp = { type: 'api', page: location.href };
+  _.extend(temp, terminalInfo, ext);
   send(temp);
 }
-export{uploadUserData}
+
+// 发送js错误数据
+function sendJsErrData(ext) {
+  var temp = { type: 'js', page: location.href };
+  _.extend(temp, terminalInfo, ext);
+  send(temp);
+}
+
+// 发送数据
+function send(param) {
+  console.log(param, 123);
+  let img = new Image();
+  img.src = window.__ml && window.__ml.config.imgUrl + params(param);
+  img.onload = img.onerror = function () {
+    img = undefined;
+  };
+}
+
+export { uploadUserData }
