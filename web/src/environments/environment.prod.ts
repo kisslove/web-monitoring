b19ab0732829d70c89c6596a0b7e54eebe94d98f
hubing <676022504@qq.com>
2018-09-13 22:18:52 +0800

    1

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   apiUrl:'http://hubing.online:8081/',
-  uploadDataUrl:'http://hubing.online:8081/Monitor/Up',
+  uploadDataUrl:'http://hubing.online:8081/Up',
   jsHackUrl:'http://hubing.online:8082/bundle.js'
 };
