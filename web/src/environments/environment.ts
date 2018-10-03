9863716c3fb086c7009ab77c718d9fe6ef876ef9
hubing <676022504@qq.com>
2018-10-03 09:39:41 +0800

    发布

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,8 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
+  // production: false,
+  // apiUrl:'http://localhost:9000/',
+  // uploadDataUrl:'http://localhost:9000/Up',
+  // jsHackUrl:'http://localhost:9002/bundle.js'
   production: false,
-  apiUrl:'http://localhost:9000/',
-  uploadDataUrl:'http://localhost:9000/Up',
-  jsHackUrl:'http://localhost:9002/bundle.js'
+  apiUrl:'http://hubing.online:8081/',
+  uploadDataUrl:'http://hubing.online:8081/Up',
+  jsHackUrl:'http://hubing.online:8082/bundle.js'
 };
