1d38d401c57b819b838351dcb4406f5bb7ae92f0
hub <Esri0408>
2020-11-15 21:14:06 +0800

    update

--- web/src/app/homepage/home-page/homepage.component.ts
+++ web/src/app/homepage/home-page/homepage.component.ts
@@ -1,35 +1,35 @@
-import { debounceTime } from 'rxjs/operators';
-import { Broadcaster } from './../../monitor.common.service';
-import { CookieService } from 'ngx-cookie-service';
-import { Router } from '@angular/router';
-import { NzMessageService } from 'ng-zorro-antd';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { debounceTime } from "rxjs/operators";
+import { Broadcaster } from "./../../monitor.common.service";
+import { CookieService } from "ngx-cookie-service";
+import { Router } from "@angular/router";
+import { NzMessageService } from "ng-zorro-antd";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, Renderer2 } from "@angular/core";
 import * as CryptoJS from "crypto-js";
-import { UserService } from '../../monitor.common.service';
-import { Subscription, fromEvent } from 'rxjs';
+import { UserService } from "../../monitor.common.service";
+import { Subscription, fromEvent } from "rxjs";
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  selector: "app-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.scss"],
 })
 export class HomepageComponent implements OnInit {
-  _KEY: "28756942659325487412569845231586" //32位
-  _IV: "8536874512548456" //16位
+  _KEY: "28756942659325487412569845231586"; //32位
+  _IV: "8536874512548456"; //16位
   model = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
   model2 = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
   isVisible_login: boolean = false;
   isVisible_register: boolean = false;
   isLogin: boolean = false;
   unsubscribe: Subscription;
-  formatOne = value => `10,000 RMB`;
+  booksData = [];
   constructor(
     private http: HttpClient,
     private msg: NzMessageService,
@@ -38,12 +38,20 @@ export class HomepageComponent implements OnInit {
     private user: UserService,
     private broadcaster: Broadcaster,
     private render: Renderer2
-  ) { }
+  ) {}
 
   ngOnInit() {
     setTimeout(() => {
       this.isLogin = this.user.getToken() ? true : false;
     }, 1000);
+
+    fetch("../../../assets/books.json").then(async (r) => {
+      let data = await r.json();
+      console.log(data, 123);
+      if (data.length > 0) {
+        this.booksData = data;
+      }
+    });
     // this.unsubscribe = fromEvent(window, "resize").pipe(
     //   debounceTime(100))
     //   .subscribe((event) => {
@@ -52,23 +60,33 @@ export class HomepageComponent implements OnInit {
   }
 
   _resizePageHeight() {
-    this.render.setStyle(document.querySelector("#home-section"), "height", document.body.clientHeight - 50 + 'px');
+    this.render.setStyle(
+      document.querySelector("#home-section"),
+      "height",
+      document.body.clientHeight - 50 + "px"
+    );
   }
 
   login() {
     let pwd = this.encrypt(this.model.password);
-    this.http.post("User/login", {
-      email: this.model.email,
-      password: pwd
-    }).subscribe((r: any) => {
-      if (r.IsSuccess) {
-        this.cookie.set("user", JSON.stringify(r.Data), new Date(new Date().setMonth(new Date().getMonth() + 1)));
-        this.broadcaster.broadcast("refreshUser");
-        this.router.navigate(['dashboard']);
-      } else {
-        this.msg.error(r.Data, { nzDuration: 4000 });
-      }
-    })
+    this.http
+      .post("User/login", {
+        email: this.model.email,
+        password: pwd,
+      })
+      .subscribe((r: any) => {
+        if (r.IsSuccess) {
+          this.cookie.set(
+            "user",
+            JSON.stringify(r.Data),
+            new Date(new Date().setMonth(new Date().getMonth() + 1))
+          );
+          this.broadcaster.broadcast("refreshUser");
+          this.router.navigate(["dashboard"]);
+        } else {
+          this.msg.error(r.Data, { nzDuration: 4000 });
+        }
+      });
   }
 
   ngOnDestroy(): void {
@@ -94,15 +112,14 @@ export class HomepageComponent implements OnInit {
   private encrypt(str) {
     var key = CryptoJS.enc.Utf8.parse(this._KEY);
     var iv = CryptoJS.enc.Utf8.parse(this._IV);
-    var encrypted = '';
+    var encrypted = "";
     var srcs = CryptoJS.enc.Utf8.parse(str);
     encrypted = CryptoJS.AES.encrypt(srcs, key, {
       iv: iv,
       mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7
+      padding: CryptoJS.pad.Pkcs7,
     }).ciphertext;
 
     return encrypted.toString();
   }
-
 }
