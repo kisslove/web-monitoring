5302224542fca5cb4ba28f2c0cb3d5a3eabe84be
胡兵 <676022504@qq.com>
2022-09-23 18:02:33 +0800

    feat: 优化上报数据处理速度，增加人物队列

--- backend_server/routes/upData.js
+++ backend_server/routes/upData.js
@@ -9,43 +9,54 @@ var focusClickInfo = require('../business/focusClickInfo');
 var consoleInfo = require('../business/consoleInfo');
 var util = require('../utils/util');
 var _ = require('lodash');
+
+let uploadDataTaskList = [];
+
+const solveData = () => {
+    const tasks = uploadDataTaskList.splice(0, 5);
+    tasks.forEach(r => {
+        switch (r.type) {
+            case 'pv':
+                pvInfo.create(r);
+                break;
+            case 'js':
+                jsError.create(r);
+                break;
+            case 'api':
+                apiInfo.create(r);
+                break;
+            case 'perf':
+                perfInfo.create(r);
+                break;
+            case 'resource':
+                resourceInfo.create(r);
+                break;
+            case 'focusClick':
+                focusClickInfo.create(r);
+                break;
+            case 'console':
+                consoleInfo.create(r);
+                break;
+            default:
+                break;
+        }
+    })
+}
+
+// 每10秒执行一次检查
+setInterval(() => {
+    solveData()
+}, 10000);
+
 /*上传数据 */
 router.get('', util.getIp, function (req, res, next) {
     let temp = JSON.parse(req.query.paramsJson);
     temp = _.extend(temp, req.netInfo);
     temp.type = req.query.type;
-    switch (req.query.type) {
-        case 'pv':
-            pvInfo.create(temp);
-            res.status(200).end();
-            break;
-        case 'js':
-            jsError.create(temp);
-            res.status(200).end();
-            break;
-        case 'api':
-            apiInfo.create(temp);
-            res.status(200).end();
-            break;
-        case 'perf':
-            perfInfo.create(temp);
-            res.status(200).end();
-            break;
-        case 'resource':
-            resourceInfo.create(temp);
-            res.status(200).end();
-            break;
-        case 'focusClick':
-            focusClickInfo.create(temp);
-            res.status(200).end();
-            break;
-        case 'console':
-            consoleInfo.create(temp);
-            res.status(200).end();
-            break;
-        default:
-            break;
+    if (temp.type) {
+        uploadDataTaskList.push(temp)
     }
+   res.status(200).end();
 });
 
 module.exports = router;
